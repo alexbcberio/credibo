dfc46314ed208d7510e60e27d5cf6d1df78543d7
Alexander BC <alexbcberio@gmail.com>
2022-03-06 14:22:07 +0100

    feat(BotClient): change bot client instantation method
    Set constructor as private, create method has to be used from now on. The method returns a proxy instance of the client so that it can be directly accessed to the added plugins as client.pluginName
    
    Instances of BotClient have to be created through its create static method

--- src/botClient/index.ts
+++ src/botClient/index.ts
@@ -11,15 +11,42 @@ import {
 import debug from "debug";
 
 class BotClient {
+  public static create(options: ClientOptions): BotClient {
+    const instance = new this(options);
+
+    const instanceProxy = new Proxy(instance, {
+      get(target: BotClient, handler: string | symbol) {
+        // TODO: find a better way to avoid @ts-expect-error
+        // @ts-expect-error cannot index target
+        const targetHandler = target[handler];
+
+        if (targetHandler) {
+          return targetHandler;
+        } else if (
+          typeof handler === "string" &&
+          target.plugins.hasPlugin(handler)
+        ) {
+          return target.plugins.getPlugin(handler);
+        }
+
+        // eslint-disable-next-line no-undefined
+        return undefined;
+      },
+    });
+
+    return instanceProxy;
+  }
+
   public readonly log: debug.Debugger;
   public readonly discord: Client;
+  public readonly helper = helper;
+
   public readonly commands: CommandManager;
   public readonly events: EventManager;
   public readonly modules: ModuleManager;
-  public readonly helper = helper;
   public readonly plugins: PluginManager;
 
-  constructor(options: ClientOptions) {
+  private constructor(options: ClientOptions) {
     this.log = debug(this.constructor.name);
     this.discord = new Client(options);
 
