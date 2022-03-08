49e0d063e0bebc5dad9e5016affd4cc7d0d4fdff
Alexander BC <alexbcberio@gmail.com>
2022-03-08 23:47:11 +0100

    chore: disable no-magic-numbers linting rule
    There is no way to fix the warning so we are going to ignore it

--- src/botClient/types/Plugin.ts
+++ src/botClient/types/Plugin.ts
@@ -3,7 +3,9 @@ import { Debugger } from "debug";
 
 abstract class Plugin {
   public readonly name =
+    // eslint-disable-next-line no-magic-numbers
     this.constructor.name.charAt(0).toLowerCase() +
+    // eslint-disable-next-line no-magic-numbers
     this.constructor.name.substring(1);
   public abstract readonly version: `${number}.${number}.${number}`;
 
